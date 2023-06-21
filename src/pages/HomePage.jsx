687b72d138d353c360eec22ddfc5da4d03200907
karen okasaki <oks.karen@gmail.com>
2023-06-20 22:15:31 -0300

    antes do tailwind / create / read / pagina de detail done!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
    const [questions, setQuestions] = useState([]);
+   const [form, setForm] = useState({
+      question: "",
+      answers: [],
+      user: "",
+   });
+   const [reload, setReload] = useState(false);
+   const [showForm, setShowForm] = useState(false);
 
    useEffect(() => {
       async function fetchQuestions() {
@@ -14,11 +21,60 @@ export default function HomePage() {
       }
 
       fetchQuestions();
-   }, []);
+   }, [reload]);
+
+   function handleChange(e) {
+      setForm({ ...form, [e.target.name]: e.target.value });
+   }
+
+   async function handleSubmit(e) {
+      e.preventDefault();
+      try {
+         const response = await axios.post(
+            "https://webdev103.cyclic.app/questions",
+            form
+         );
+
+         setReload(!reload);
+         setForm({
+            question: "",
+            asnswers: [],
+            user: "",
+         });
+      } catch (error) {
+         console.log(error);
+      }
+   }
 
    return (
       <div>
-         <h1>Home</h1>
+         <h1>Ask Me!</h1>
+
+         <button onClick={() => setShowForm(!showForm)}>
+            Faça uma pergunta!
+         </button>
+
+         {showForm === true && (
+            <form>
+               <label>Pergunta</label>
+               <textarea
+                  type="text"
+                  name="question"
+                  value={form.question}
+                  onChange={handleChange}
+               />
+
+               <label>Nome</label>
+               <input
+                  type="text"
+                  name="user"
+                  value={form.user}
+                  onChange={handleChange}
+               />
+
+               <button onClick={handleSubmit}>Perguntar!</button>
+            </form>
+         )}
 
          <div>
             {questions.map((question) => {
@@ -27,6 +83,11 @@ export default function HomePage() {
                      <Link to={`/questions/${question._id}`}>
                         <p>{question.question}</p>
                         <p>{question.user}</p>
+                        <p>
+                           {question.answers.length > 0
+                              ? `${question.answers.length} respostas`
+                              : "Ainda não teve resposta"}
+                        </p>
                      </Link>
                   </div>
                );
--- src/pages/QuestionDetail.jsx
+++ src/pages/QuestionDetail.jsx
@@ -1,7 +1,93 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
 export default function QuestionDetail() {
+   const params = useParams();
+
+   const [question, setQuestion] = useState({});
+   const [form, setForm] = useState({
+      answer: "",
+      user: "",
+   });
+   //state que vai dar o trigger no useEffect para rodar novamente e pegar as informações atualizadas
+   const [reload, setReload] = useState(false);
+
+   useEffect(() => {
+      async function fetchQuestion() {
+         const response = await axios.get(
+            `https://webdev103.cyclic.app/questions/${params.id}`
+         );
+         setQuestion(response.data);
+      }
+
+      fetchQuestion();
+   }, [reload]);
+
+   function handleChange(e) {
+      setForm({ ...form, [e.target.name]: e.target.value });
+   }
+
+   async function handleSubmit(e) {
+      e.preventDefault();
+
+      try {
+         const response = await axios.put(
+            `https://webdev103.cyclic.app/questions/${params.id}`,
+            { answers: [...question.answers, form] }
+         );
+         //pedir para o useEffect rodar novamente
+         setReload(!reload);
+         //deixar o form vazio depois da resposta
+         setForm({
+            answer: "",
+            user: "",
+         });
+      } catch (error) {
+         console.log(error);
+      }
+   }
+
+   console.log(question);
+
    return (
       <div>
-         <h1>Question Detail</h1>
+         <h1>{question.question}</h1>
+         <p>Autor: {question.user}</p>
+         <button>Editar pergunta</button>
+
+         <div>
+            {question.answers &&
+               question.answers.map((answer) => {
+                  return (
+                     <div key={answer.answer}>
+                        <p>{answer.answer}</p>
+                        <p>{answer.user}</p>
+                     </div>
+                  );
+               })}
+         </div>
+
+         <form>
+            <label>Resposta</label>
+            <textarea
+               type="text"
+               rows={5}
+               name="answer"
+               value={form.answer}
+               onChange={handleChange}
+            />
+
+            <label>Nome</label>
+            <input
+               type="text"
+               name="user"
+               value={form.user}
+               onChange={handleChange}
+            />
+
+            <button onClick={handleSubmit}>Salvar Resposta</button>
+         </form>
       </div>
    );
 }
